46ccf0b9ce50bffe955a3a3c298a5d6199440791
Jesus Ortiz <jesusestiff@gmail.com>
2022-10-17 14:18:33 -0600

    commit tree 3929a767212ce29d25d4bd8cef45a60c34ba2884

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo", "Otis" , "Garfield"] 
+   function destructivelyAppendCat(name){
+        cats.push(name) }
+            function destructivelyPrependCat(name){
+                cats.unshift(name)}
+                    function destructivelyRemoveLastCat(Garfield){
+                        cats.pop(Garfield)}
+                        function destructivelyRemoveFirstCat(Milo){
+                            cats.shift(Milo)}
+                                function appendCat(name){ 
+                                    const cat2 = [...cats, name] 
+                                         return cat2} 
+
+                                function prependCat(name){
+                                    const cat2 = [name ,...cats] 
+                                        return cat2 
+                                }
+                                function removeLastCat(Garfield){
+                                    const cat2 =[...cats,] 
+                                        cat2.pop(Garfield) 
+                                        return cat2 }
+                                    function removeFirstCat(Milo){
+                                        const cats2 =[...cats] 
+                                        cats2.shift(Milo) 
+                                        return cats2
+                                    }
